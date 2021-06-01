2f2ef0c7a97bf68ae8bc1f10bc5d82224bad1b6f
Augusto Renan <augustorenanss@gmail.com>
2021-06-01 13:51:38 -0300

    fix test 'Shold return 400 if no email is provided'

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 const LoginRouter = require('./login-router')
-const MissingParamError = require('../helpers/unauthorized-error')
 const UnauthorizedError = require('../helpers/unauthorized-error')
+const MissingParamError = require('../helpers/missing-param-error')
 
 const makeSut = () => {
   class AuthUseCaseSpy {
@@ -75,5 +75,17 @@ describe('Login Router', () => {
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
+  })
+
+  test('Should return 500 if no AuthUseCase is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
